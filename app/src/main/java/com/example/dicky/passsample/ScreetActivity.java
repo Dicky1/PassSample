80d1d140b5131ea32bd01b49dc6d24e245fb34b4
Dicky1 <dsetiono1@gmail.com>
2018-03-14 02:10:30 +0700

    Aplikasi samsung tizen

---
+++ app/src/main/java/com/example/dicky/passsample/ScreetActivity.java
@@ -0,0 +1,13 @@
+package com.example.dicky.passsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screet);
+    }
+}
